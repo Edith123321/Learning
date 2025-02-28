c566d50e5b4929625ce0b2a841f7243c52559bd3
edith.githinji <edithgithinji2020@gmail.com>
2025-02-28 22:58:57 +0200

    Hero section complete

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import assets from '../../assets/assets/assets'
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className=' flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+      <h1 className='md:text-home-heading-large text-home-heading-medium relative font-bold text-gray-800 max-w-3xl mx-auto'>Empower your future with courses designed to <span className='text-blue-600 '>fit your choice</span><img src={assets.sketch} alt="" className='md:block hidden absolute -bottom-7 right-0'/></h1>
+
+      <p>We bring together world-class instructiors, interactive content and supportive community to help you achieve your professional goals. </p>
     </div>
   )
 }
